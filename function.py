cef1d71a0c577b3c8bbd0ab58fbfcdd48a317648
cyshadow1607 <132059660+cyshadow1607@users.noreply.github.com>
2023-05-13 14:17:27 +0530

    Add files via upload
    To create a function called most_frequent that takes a string and prints the letters in decreasing order of frequency

---
+++ function.py
@@ -0,0 +1,19 @@
+a=str(input("Enter a word"))
+def make_dict(x):
+    dictionary = {}
+    for letter in x:
+        dictionary[letter] = 1 + dictionary.get(letter, 0)
+    return dictionary
+
+
+def most_frequent(a):
+    letters = [letter.lower() for letter in a if letter.isalpha()]
+    dictionary = make_dict(letters)
+    result = []
+    for key in dictionary:
+        result.append((dictionary[key], key))
+    result.sort(reverse=True)
+    for count, letter in result:
+        print (letter, count)
+
+most_frequent(a)
